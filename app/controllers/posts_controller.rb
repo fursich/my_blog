f89cca043f61ae122d9d2034e11dcc69e79e36a7
Koji <kozzzzy@gmail.com>
2016-11-27 09:31:51 +0000

    WIP

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @new_posts = Post.all
   end
 
   def show
@@ -19,9 +20,14 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    
+    redirect_to root_path
   end
 
   def destroy
