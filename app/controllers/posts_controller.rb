7884c5b4557562e74a5bcb4441c9182c4b69a20d
Koji <kozzzzy@gmail.com>
2016-11-28 06:50:46 +0000

    kaminariとransack検索機能追加した

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,15 @@
 class PostsController < ApplicationController
+
+  before_action :set_post, only: [:show,:edit,:update,:destroy]
+
   def index
-    @posts = Post.all
-    @new_posts = Post.all
+    @q = Post.order(created_at: :desc).ransack(params[:q])
+    @posts = @q.result.page(params[:page]).per(2)
+    # 最新記事を５つ取得
+    @new_posts = Post.order(created_at: :desc).limit(5)
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def new
@@ -16,25 +20,28 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     @post.save
     
-    redirect_to root_path
+    redirect_to "/posts/#{@post.id}"
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     @post.update(post_params)
     
-    redirect_to root_path
+    redirect_to "/posts/#{@post.id}"
   end
 
   def destroy
+    @post.destroy
+    
+    redirect_to root_path
   end
   
   private
-  
+  def set_post
+    @post = Post.find(params[:id])
+  end
   def post_params
     params.require(:post).permit(:title, :body, :category)
   end
